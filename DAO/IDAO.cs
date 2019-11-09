5c334fb94bb59f335ce78a239a503ed0db44e59c
Марчёнок Влад <vladmarchenok@mail.ru>
2019-11-09 17:22:46 +0300

    Добавлен интерфейс IDAO

---
+++ DAO/IDAO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Computer_Store.DAO
+{
+    interface IDAO
+    {
+        void Create<T>(T t);
+        List<T> GetAll<T>();
+        T GetOne<T>(int id);
+        void Update<T>(int id, T t);
+        void Delete<T>(int id);
+    }
+}
